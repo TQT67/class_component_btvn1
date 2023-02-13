cb98b921afb2e62fec141444d983f3b578afe752
TQT67 <trangtrinh000000@gmail.com>
2023-02-13 14:29:56 +0700

    btvn1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Task extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      tasks: [],
+    };
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEditTask = this.handleEditTask.bind(this);
+    this.handleDeleteTask = this.handleDeleteTask.bind(this);
+  }
+
+  handleChangeName(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const task = {
+      name: this.state.name,
+    };
+    this.setState({
+      tasks: [...this.state.tasks, task],
+      name: "",
+    });
+  }
+
+  handleEditTask(e, index) {
+    const tasks = this.state.tasks.slice();
+    tasks[index].name = e.target.value;
+    this.setState({ tasks });
+  }
+
+  handleDeleteTask(index) {
+    const tasks = this.state.tasks.slice();
+    tasks.splice(index, 1);
+    this.setState({ tasks });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Viết nội dung </h3>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+          <button type="submit">Thêm Task</button>
+        </form>
+        <ul>
+          {this.state.tasks.map((task, index) => (
+            <li key={index}>
+              <input
+                type="text"
+                value={task.name}
+                onChange={(e) => this.handleEditTask(e, index)}
+              />
+              <button onClick={() => this.handleDeleteTask(index)}>Xóa Task</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Task;
